fb42967192d7288010921f149f6b399895d7154b
Thomas <tg.59290@gmail.com>
2020-01-17 16:24:08 +0100

    jquery working on header

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,7 @@
 $(".landing h1, .landing h2").hide();
 
 // cascade apparition for h1 and h2 with delay
-$(".landing h1").delay(500).fadeIn(1500,function showNext(){
+$(".landing h1").delay(500).fadeIn(1500, function(){
   $(".landing h2").slideToggle();
 });
 
