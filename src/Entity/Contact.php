3f6963f8093ae4f9b96746941f17476f6c512234
Thomas <tg.59290@gmail.com>
2020-01-30 15:18:55 +0100

    add awful reggex on contact message attribut to prevent from adds and spam without captcha

--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -33,8 +33,13 @@ class Contact
    * @var string
    * @Assert\NotBlank
    * @Assert\Length(
-   *      min = 20,
+   *      min = 50,
    *      minMessage = "Votre message doit contenir au moins {{ limit }} lettres",
+   * )
+   * @Assert\Regex(
+   *     pattern="/\bsexe|\bsex|\bhot girl(s)?|\bfuck|\bdick|\blonely girl(s)?|\bfille(s)? célibatire(s)?|\bmerde|\bbite|\bconnard|\bpute|\bcaca|\beasy cash|\bargent facil(ement)?|\bargent rapid(ement)?|\bdevenir riche|\bmillionaire|\beasy money/i",
+   *     match=false,
+   *     message="Le contenu de votre message semble étrange"
    * )
   */
   private $message;
