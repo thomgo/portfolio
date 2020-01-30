843e5d073e4a3cbad89a71321c45f7ce8e194b5a
Thomas <tg.59290@gmail.com>
2020-01-30 16:37:09 +0100

    add test to check there is a h1 h2 and h3 on every front page

--- tests/Controller/FrontControllerTest.php
+++ tests/Controller/FrontControllerTest.php
@@ -17,6 +17,19 @@ class FrontControllerTest extends WebTestCase
     $this->assertTrue($client->getResponse()->isSuccessful());
   }
 
+  /**
+  * @dataProvider provideUrls
+  */
+  public function testPageContainsTitles($url)
+  {
+    $client = self::createClient();
+    $crawler = $client->request('GET', $url);
+
+    $this->assertCount(1, $crawler->filter('h1'));
+    $this->assertCount(1, $crawler->filter('h2'));
+    $this->assertCount(1, $crawler->filter('h3'));
+  }
+
   public function provideUrls()
   {
     return [
